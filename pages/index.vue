607baf80d1320d9fd9f113e59a77c442fe0818c5
Chrisimir <chrislund97@gmail.com>
2020-05-11 17:41:20 +0200

    remove FrenaLaCurva map

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,6 @@
         <WhatWeDo />
         <ValuesContainer />
         <Facts />
-        <MapFrenaLaCurva />
         <Manifest />
         <Contact />
     </div>
@@ -14,7 +13,6 @@
 import MainHero from '@/components/mainHero'
 import WhatWeDo from '@/components/whatWeDo'
 import ValuesContainer from '@/components/valuesContainer'
-import MapFrenaLaCurva from '@/components/mapFrenaLaCurva'
 import Facts from '@/components/facts'
 import Manifest from '@/components/manifest'
 import Contact from '@/components/contact'
@@ -23,7 +21,6 @@ export default {
         MainHero,
         WhatWeDo,
         ValuesContainer,
-        MapFrenaLaCurva,
         Facts,
         Manifest,
         Contact

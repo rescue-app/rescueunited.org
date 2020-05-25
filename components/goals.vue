ef7297ede9df9f31e6c71f048a1b7b2d580365db
Chrisimir <chrislund97@gmail.com>
2020-05-25 18:31:34 +0200

    updated subtext

--- components/goals.vue
+++ components/goals.vue
@@ -3,6 +3,7 @@
         <div class="section-head text-center wide-lg pb-0">
             <h5 class="subtitle">Sustainable Development Goals</h5>
             <h2 class="title">The challenge</h2>
+            <p class="pb-4">The 2030 Agenda for Sustainable Development, adopted by all United Nations Member States in 2015, provides a shared blueprint for peace and prosperity for people and the planet, now and into the future.</p>
         </div>
         <div id="goal-list">
             <nuxt-link class="goalbox" to="/"><div><img src="https://sustainabledevelopment.un.org/content/images/sdgsfirst.png" alt="Goal"></div></nuxt-link>
@@ -25,9 +26,8 @@
             <nuxt-link class="goalbox" to="/"><div><img src="https://sustainabledevelopment.un.org/content/images/E_SDG_Icons-17.jpg" alt="Goal"><div class="goaltext" style="background: #14496a">Strengthen the means of implementation and revitalize the global partnership for sustainable development</div></div></nuxt-link>
         </div>
         <div id="explainer"  class="pt-3">
-            <p>The 2030 Agenda for Sustainable Development, adopted by all United Nations Member States in 2015, provides a shared blueprint for peace and prosperity for people and the planet, now and into the future.</p>
             <p>At its heart are the 17 Sustainable Development Goals (SDGs), which are an urgent call for action by all countries in a global partnership.</p>
-            
+
             <h4 class="pt-2">The SDGs will stimulate action over the next years in areas of critical importance for humanity and the planet:</h4>
             <h5>People</h5>
             <p>            We are determined to end poverty and hunger, in all their forms and dimensions, and to ensure that all human beings can fulfil their potential in dignity and equality and in a healthy environment.</p>

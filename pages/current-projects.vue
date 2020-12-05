2f55eb222d57de332f02d9f8ba46a31d9a1145a2
almudenab <almu.ibs@gmail.com>
2020-12-05 11:08:49 +0000

    Trying some scrappy code to have projects show up in a simple table

--- pages/current-projects.vue
+++ pages/current-projects.vue
@@ -9,6 +9,49 @@
                             <h3 class="title text-center">CURRENT PROJECTS</h3>
                             <p class="lead mb-0 text-center">These are our current projects. You can <strong><nuxt-link to="/propose-a-project">propose a new one</nuxt-link></strong>, or <strong><nuxt-link to="/volunteering">sign up as a volunteer</nuxt-link></strong></p>
 
+                            <div class="text-block">
+                                <p>
+                                    <table style="width:800px; border: 0px; margin-left:auto; margin-right:auto; padding:20px; border-spacing:20px"
+                                        <tr>
+                                            <th>Rescue app</th>
+                                            <th>Calmtag</th>
+
+                                        </tr>
+                                        <tr>
+                                            <th>Rescue app started as a response to COVID-19, due to the decentralisation of information about supply and demand of sanitary resources in Spain.<br> 
+
+                                            We created a platform to centralise and match supply and demand of sanitary resources.<br> 
+
+                                            On one side, hospitals, elderly residences or individuals request what they need.<br> 
+
+                                            In the other side, companies or people producing those materials register what they can donate.<br> 
+
+                                            Then we prioritise and match those resources with who most needs them.<br> 
+
+                                            From idea to launch, took us 3 days and in just a month we received 46,000+ offers, 74,000+ requests and we were able to match 36,000+ sanitary resources.<br> 
+
+                                            With Rescue app, we learned that if you put a group of people with different skills together, organise them in a specific way, and apply technology to the agreed solution, they can exponentially increase their impact on any social cause.<br> 
+
+                                            We need support in Sales and Account Management.<br> 
+
+                                            www.rescueapp.<br>
+                                            </th>
+                                            <th>Our mission is to offer security & health integrated solution, affordable for anyone with a dependent person on his/her family.<br> 
+
+                                            Calm Tag is a clothing label, simple and easily integrated into clothing, designed for elderly dependents and small children, which facilitates their location in case of loss, simply by reading it with a mobile phone.<br> 
+
+                                            http://calmtag.es/
+                                            </th>
+
+                                        </tr>
+                                    </table>
+
+
+
+
+
+
+
                             <div class="col" v-if="loading">
                                 <div class="spinner-border spinner-border-sm" role="status"></div>&nbsp;
                                 <span class="font-weight-bold">Loading...</span>
@@ -43,32 +86,7 @@
     </div>
 </template>
 
-<script>
-import SmallHero from '@/components/smallHero'
 
-export default {
-    components: {
-        SmallHero,
-    },
-    async fetch () {
-        const requestUrl = `https://api.rescueunited.org/project?limit=50`
-        try {
-            this.projects = await this.$http.$get(requestUrl);
-        } catch (ex) {
-            this.projects = false
-        } finally {
-            this.loading = false
-        }
-    },
-    data () {
-        return {
-            projects: [],
-            loading: true,
-        }
-    },
-    fetchOnServer: false,
-}
-</script>
 
 <style scoped>
 .search-result:not(:last-child) {
